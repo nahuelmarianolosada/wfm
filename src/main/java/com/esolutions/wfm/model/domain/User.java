555bfdaf8020b9efcb84aaadad6df7d2e6389240
nahuelmarianolosada <yosoyyo.nahuel@gmail.com>
2018-02-09 00:45:07 -0300

    UserRestController workforce/users

---
+++ src/main/java/com/esolutions/wfm/model/domain/User.java
@@ -0,0 +1,50 @@
+package com.esolutions.wfm.model.domain;
+
+/**
+ * Created by root on 03/02/18.
+ */
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+    //getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+
+}
+
