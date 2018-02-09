2385cd76720b1448d7583cba7239b14cd9673409
nahuelmarianolosada <yosoyyo.nahuel@gmail.com>
2018-02-09 04:22:56 -0300

    UserRestController workforce/users

--- src/main/webapp/WEB-INF/views/wfm-front/src/app/app.module.ts
+++ src/main/webapp/WEB-INF/views/wfm-front/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user/user-list/user-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
